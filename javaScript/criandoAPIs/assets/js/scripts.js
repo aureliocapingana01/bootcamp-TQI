0d0094694966a9c070faab50a74489df50ac90b8
Aurelio Capingana <aurelio.capingana01@gmail.com>
2022-07-12 11:09:30 -0300

    API

---
+++ javaScript/criandoAPIs/assets/js/scripts.js
@@ -0,0 +1,55 @@
+const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+const btn = document.getElementById('change-cat');
+
+const getCats = async () => {
+	try {
+		const data = await fetch(BASE_URL);
+		const json = await data.json();
+		return json.webpurl;
+	} 
+	catch (e) {
+		console.log(e.message);
+	}
+};
+
+const loadImg = async () => {
+	const catImg = document.getElementById('cat')
+	catImg.src = await getCats()
+};
+
+
+// const btn = document.getElementById('change-cat');
+btn.addEventListener('click', loadImg);
+
+loadImg();
+
+
+
+// const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
+// const catBtn = document.getElementById(`change-cat`)
+// // const catImg = document.getElementById('cat')
+
+// const getCats = async () => {
+// 	try {
+// 		const data = await fetch(BASE_URL);
+// 		const json = await data.json();
+// 		return json.webpurl;
+// 	} catch (e) {
+// 		console.log(e.message);
+// 	}
+// };
+
+// const loadImg = async () => {
+//     const catImg = document.getElementById('cat');
+//     catImg.src = await getCats();
+
+
+// 	// const img = document.getElementsByTagName('img')[0];
+// 	// img.src = await getCats();
+// };
+// catBtn.addEventListener('click', loadImg);
+
+// loadImg();
+
+// const btn = document.getElementById('change-cat');
+// btn.addEventListener('click', loadImg);
